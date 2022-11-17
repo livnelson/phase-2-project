333b0cc2df344742dedaaa44c680047052be2b97
Michelle Too <mjxtcodes@gmail.com>
2022-11-17 09:48:52 -0800

    update expenselist

--- budget/src/App.js
+++ budget/src/App.js
@@ -5,7 +5,12 @@ import Remaining from './Remaining';
 import ExpenseTotal from './ExpenseTotal';
 import ExpenseList from './ExpenseList';
 import AddExpenseForm from './AddExpenseForm'
+<<<<<<< HEAD
 import {AppProvider} from './AppContext';
+=======
+import { AppProvider } from "./AppContext.js"
+
+>>>>>>> 66f7177 (fix bugs)
 import "./global.css"
 
 const App = () => {
@@ -14,6 +19,7 @@ const App = () => {
 	
 	return (
 		<AppProvider>
+<<<<<<< HEAD
 			<div className='container'>
 				<h1 className='mt-3'>My Budget Planner</h1>
 				<div className='row mt-3'>
@@ -25,6 +31,19 @@ const App = () => {
 					</div>
 					<div id="expense-col" className='col-sm'>
 						<ExpenseTotal />
+=======
+		<div className='container'>
+			<h1 className='mt-3'>My Budget Planner</h1>
+			<div className='row mt-3'>
+				<div id="budget-col" className='col-sm'>
+					<Budget />
+				</div>
+				<div id="remaining-col"className='col-sm'>
+					<Remaining />
+				</div>
+				<div id="expense-col"className='col-sm'>
+					<ExpenseTotal />
+>>>>>>> 66f7177 (fix bugs)
 					</div>
 				</div>
 				<h3 className='mt-3'>Expenses</h3>
--- budget/src/ExpenseList.js
+++ budget/src/ExpenseList.js
@@ -1,15 +1,29 @@
-import React, {useContext} from 'react';
+import React, {useState, useEffect} from 'react';
 import ExpenseItem from './ExpenseItem'
 import { AppContext } from './AppContext';
 
 const ExpenseList = () => {
-    const {expenses} = useContext(AppContext)
-    
+    // const {expenses} = useContext(AppContext)
+
+    const [expenses, setExpenses] = useState([])
+	const expensesArray = useEffect(() =>
+    {fetch('http://localhost:8000/expenses')
+			.then((res) => res.json())
+			.then(expensesData => setExpenses(expensesData))}, [])
+
+    const mappedExpenses = expenses.map((expense) => (
+        <ExpenseItem 
+            key={expense.id} 
+            id={expense.id}
+            name={expense.name} 
+            cost={expense.cost}
+            // handleEdit={handleEdit}
+        />
+    ))
+
     return (
         <ul className='list-group'>
-            {expenses.map((expense) => (
-                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} />
-            ))}
+            {mappedExpenses}
         </ul>
     )
 }
